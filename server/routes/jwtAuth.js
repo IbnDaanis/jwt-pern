1152150e61df3d921d7db9fb3df1566e9926c57a
Ikraam <IbnDaanis@Gmail.com>
2021-07-02 16:50:39 +0100

    sign up route if user exists

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,5 +1,24 @@
 import { Router } from 'express'
+import { pool } from '../database/database.js'
 
 const router = Router()
+
+router.post('/register', async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body
+
+    const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [email])
+
+    if (user) {
+      return res.status(400).send('User already exists, choose a different email address')
+    }
+
+    res.json(user.rows[0])
+    next()
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+})
 
 export default router
