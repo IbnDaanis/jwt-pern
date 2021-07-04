bb81f1f243c14aa9dce2f3371563dfb7f4390c13
Ikraam <IbnDaanis@Gmail.com>
2021-07-04 19:00:57 +0100

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,13 @@ import { Register } from './components/Register'
 export const App = () => {
   return (
     <Router>
-      <div className='container'></div>
+      <div className='container'>
+        <Switch>
+          <Route exact path='/login' render={props => <Login {...props} />} />
+          <Route exact path='/register' render={props => <Register {...props} />} />
+          <Route exact path='/dashboard' render={props => <Dashboard {...props} />} />
+        </Switch>
+      </div>
     </Router>
   )
 }
