ae4247bb542cc7143f71ceff8ecdc1ae300107ba
Ikraam <IbnDaanis@Gmail.com>
2021-07-04 18:28:03 +0100

    select user name for dashboard

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,9 +4,11 @@ import { authorization } from '../middleware/authorization.js'
 
 const router = Router()
 
-router.get('/', authorization, (req, res) => {
+router.get('/', authorization, async (req, res) => {
   try {
-    res.status(200).json(req.user)
+    const user = (await pool.query('SELECT user_name FROM users where user_id = $1', [req.user]))
+      .rows[0]
+    res.status(200).json(user)
   } catch (error) {
     console.error(error.message)
     res.status(500).send('Server Error')
