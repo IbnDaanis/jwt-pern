547a8c88421871743845ffc1aefd9e20cba84221
Ikraam <IbnDaanis@Gmail.com>
2021-07-04 18:53:53 +0100

    react dom and toastify

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { Dashboard } from './components/Dashboard'
+import { Login } from './components/Login'
+import { Register } from './components/Register'
 
 export const App = () => {
   return (
     <Router>
-      <Switch></Switch>
+      <div className='container'></div>
     </Router>
   )
 }
