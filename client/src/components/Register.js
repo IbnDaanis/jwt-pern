4dbd9e4d45b1a82eb0aaa24354ba8167c2d9dab0
Ikraam <IbnDaanis@Gmail.com>
2021-07-05 16:06:20 +0100

    Register setAuth

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
-export const Register = () => {
-  return <div>Register</div>
+export const Register = ({ setIsAuthenticated }) => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <button onClick={() => setIsAuthenticated(true)}>Sign Up</button>
+    </div>
+  )
 }
