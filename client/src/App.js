5cc2013deadef8f45670cf22298f90830920ba9c
Ikraam <IbnDaanis@Gmail.com>
2021-07-05 16:03:34 +0100

    authentication redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import { Dashboard } from './components/Dashboard'
 import { Login } from './components/Login'
 import { Register } from './components/Register'
 
 export const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
   return (
     <Router>
       <div className='container'>
         <Switch>
-          <Route exact path='/login' render={props => <Login {...props} />} />
-          <Route exact path='/register' render={props => <Register {...props} />} />
-          <Route exact path='/dashboard' render={props => <Dashboard {...props} />} />
+          <Route
+            exact
+            path='/login'
+            render={props =>
+              !isAuthenticated ? (
+                <Login {...props} setIsAuthenticated={setIsAuthenticated} />
+              ) : (
+                <Redirect to='/dashboard' />
+              )
+            }
+          />
+          <Route
+            exact
+            path='/register'
+            render={props =>
+              !isAuthenticated ? (
+                <Register {...props} setIsAuthenticated={setIsAuthenticated} />
+              ) : (
+                <Redirect to='/dashboard' />
+              )
+            }
+          />
+          <Route
+            exact
+            path='/dashboard'
+            render={props =>
+              isAuthenticated ? (
+                <Dashboard {...props} setIsAuthenticated={setIsAuthenticated} />
+              ) : (
+                <Redirect to='/login' />
+              )
+            }
+          />
         </Switch>
       </div>
     </Router>
