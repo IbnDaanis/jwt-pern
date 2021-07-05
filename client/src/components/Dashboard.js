ad6651782c326aa070e77e98ed004c4fa8e9726f
Ikraam <IbnDaanis@Gmail.com>
2021-07-05 16:05:09 +0100

    dashboard setAuth

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export const Dashboard = () => {
+export const Dashboard = ({ setIsAuthenticated }) => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <button onClick={() => setIsAuthenticated(false)}>Logout</button>
     </div>
   )
 }
