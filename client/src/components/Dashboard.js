ec716d7bcc63c39b0b54a62c2b707ac240c5e634
Ikraam <IbnDaanis@Gmail.com>
2021-07-06 21:04:21 +0100

    dashboard fetch name

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const Dashboard = ({ setIsAuthenticated }) => {
+  const [name, setName] = useState('')
+
+  async function getName() {
+    try {
+      const response = await fetch('http://localhost:5000/dashboard', {
+        method: 'GET',
+        headers: {
+          token: JSON.parse(localStorage.getItem('token'))
+        }
+      })
+      const data = await response.json()
+
+      console.log(data)
+      setName(data.user_name)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getName()
+  }, [])
+
   return (
-    <div>
+    <div className='container'>
       <h1>Dashboard</h1>
-      <button onClick={() => setIsAuthenticated(false)}>Logout</button>
+      <div>
+        {name && <h2>Welcome back {name}</h2>}
+
+        <button className='btn'>Logout</button>
+      </div>
     </div>
   )
 }
