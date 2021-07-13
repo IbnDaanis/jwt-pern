a3da6f1fb3d0557a743f9b466154f1b77b48c47a
Ikraam <IbnDaanis@Gmail.com>
2021-07-14 00:38:09 +0100

    payload update

--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -3,6 +3,6 @@ import { config } from 'dotenv'
 config()
 
 export const jwtGenerator = user_id => {
-  const payload = { user: user_id }
+  const payload = { user: { id: user_id } }
   return jwt.sign(payload, process.env.jwtsecret, { expiresIn: '1hr' })
 }
