999c9b33b05df253c5a4257a7abcf235daa28c22
Ikraam <IbnDaanis@Gmail.com>
2021-07-14 00:41:24 +0100

    updated query

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -6,8 +6,9 @@ const router = Router()
 
 router.get('/', authorization, async (req, res) => {
   try {
-    const user = (await pool.query('SELECT user_name FROM users where user_id = $1', [req.user.id]))
-      .rows[0]
+    const user = (
+      await pool.query('SELECT * FROM users INNER JOIN todos ON users.user_id = todos.user_id')
+    ).rows[0]
     res.status(200).json(user)
   } catch (error) {
     console.error(error.message)
