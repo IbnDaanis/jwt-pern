d065964afc6acddd3d7dcf63984349ecd54929aa
Ikraam <IbnDaanis@Gmail.com>
2021-07-14 00:39:09 +0100

    updated req.user.id

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -6,7 +6,7 @@ const router = Router()
 
 router.get('/', authorization, async (req, res) => {
   try {
-    const user = (await pool.query('SELECT user_name FROM users where user_id = $1', [req.user]))
+    const user = (await pool.query('SELECT user_name FROM users where user_id = $1', [req.user.id]))
       .rows[0]
     res.status(200).json(user)
   } catch (error) {
