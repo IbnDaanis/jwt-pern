b317afb6ef533a366712ec4e43d510c293dca1bc
Ikraam <IbnDaanis@Gmail.com>
2021-07-14 14:37:28 +0100

    update and delete todo

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -36,4 +36,44 @@ router.post('/todos', authorization, async (req, res) => {
   }
 })
 
+router.put('/todos/:id', authorization, async (req, res) => {
+  try {
+    const { id } = req.params
+    const { description } = req.body
+
+    const updateTodo = (
+      await pool.query(
+        'UPDATE todos SET description = $1 WHERE todo_id = $2 AND user_id = $3 RETURNING *',
+        [description, id, req.user.id]
+      )
+    ).rows[0]
+
+    if (!updateTodo) return res.json('This todo is not yours')
+
+    res.json(updateTodo)
+  } catch (error) {
+    console.error(error.message)
+    res.json(error.message)
+  }
+})
+
+router.delete('/todos/:id', authorization, async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleteTodo = (
+      await pool.query('DELETE FROM todos WHERE todo_id = $1 AND user_id = $2 RETURNING *', [
+        id,
+        req.user.id
+      ])
+    ).rows[0]
+
+    if (!deleteTodo) return res.json('This todo is not yours')
+
+    res.json('Todo was deleted')
+  } catch (error) {
+    console.error(error.message)
+    res.json(error.message)
+  }
+})
+
 export default router
